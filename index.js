be936dc58211f01546b9cd38d35419349e26f92e
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-16 16:21:41 +0300

    create an arrow function

---
+++ index.js
@@ -0,0 +1,13 @@
+const PlayMusic=(music)=>{
+    return "playing some" + music;
+
+};
+var myFuncttion=(a)=>a*5;
+console.log(myFuncttion(10));
+
+const readBooks=(book1,book2)=>{
+    return( `I read ${book1} and ${book2}`);
+}
+
+const readBook = (book1, book2) => `I read '${book1}' and '${book2}'`;
+readBooks("The Old Man and the Sea", "1984");
